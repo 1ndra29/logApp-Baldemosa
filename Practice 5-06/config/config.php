6cd90630a323697ca2a83bc0789bdba7f593a215
1ndra29 <72970374+1ndra29@users.noreply.github.com>
2021-06-01 19:42:04 +0800

    Update config.php

--- Practice 5-06/config/config.php
+++ Practice 5-06/config/config.php
@@ -1,9 +1,9 @@
-<?php
+c<?php
 	define('ROOT_URL', '');
-	define('DB_HOST', '');
-	define('DB_USER', '');
-	define('DB_PASS', '');
-	define('DB_NAME', '');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6416352');
+	define('DB_PASS', 'Fe1DIJre13');
+	define('DB_NAME', 'sql6416352');
 
 //Database connection with mysqli
 $mySqli = new mysqli("localhost","root","","test");
